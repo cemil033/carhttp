6af3311478013a51cd1e5b2c337ec736b3f35799
cemil <cemiw033@gmail.com>
2022-08-01 16:57:10 +0400

    Users Http

---
+++ CarHttpServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using CarHttpServer;
+using Newtonsoft.Json;
+using System.Net;
+
+
+List<User> users = new List<User>()
+{
+    new User(){ Name = "UserOne",Surname="UserOne",Id=1},
+    new User(){ Name = "User2",Surname="User2",Id=2},
+    new User(){ Name = "User3",Surname="User3",Id=3},
+    new User(){ Name = "User4",Surname="User4",Id=4},
+    new User(){ Name = "User5",Surname="User5",Id=5}
+};
+HttpListener listener = new();
+listener.Prefixes.Add("http://localhost:63291/");
+listener.Start();
+while (true)
+{
+    var context = listener.GetContext(); 
+    var request = context.Request;
+    var response = context.Response;
+    if (request.HttpMethod == "GET")
+    {
+        StreamWriter sw = new(response.OutputStream);
+        sw.WriteLine(JsonConvert.SerializeObject(users));
+        sw.Close();
+    }
+    else if (request.HttpMethod == "POST")
+    {
+        StreamReader sr = new(request.InputStream);
+        var cnt = sr.ReadToEnd();
+        users.Add(JsonConvert.DeserializeObject<User>(cnt));
+        sr.Close();
+
+        StreamWriter sw = new(response.OutputStream);
+        sw.WriteLine("OK");
+        sw.Close();
+    }
+
+}
---
+++ UserHttpClient/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using CarHttpServer;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserHttpClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public HttpClient client=new HttpClient();
+        
+        public MainWindow()
+        {
+            InitializeComponent();           
+
+        }
+
+        private  void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var user =new User() { Id=int.Parse(tb_id.Text),Name=tb_name.Text,Surname=tb_surname.Text};
+            var json=JsonConvert.SerializeObject(user);
+            var t = client.PostAsync("http://localhost:63291/", new StringContent(json, Encoding.UTF8, "application/json"));
+            var stringContent = t.Result.Content.ReadAsStringAsync().Result;
+            MessageBox.Show(stringContent);
+        }
+
+        List<User> users = new List<User>();
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var t = client.GetAsync("http://localhost:63291/");
+            var stringContent = t.Result.Content.ReadAsStringAsync().Result;
+            users=JsonConvert.DeserializeObject<List<User>>(stringContent);
+            Lst_t.ItemsSource = users;
+        }
+    }
+}
